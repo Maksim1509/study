b4313bc2c6af2b82daefa2f4d20a7d55e1f0f968
Maksim1509 <maaa23@mail.ru>
2020-11-28 22:35:43 +0400

    Fix linter errors

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,7 +3,7 @@ module Exercise
     class << self
       def replace(array)
         max_num = array.max
-        array.map! { |num| num > 0 ? max_num : num }
+        array.map! { |num| num.positive? ? max_num : num }
       end
 
       def search(_array, _query)
