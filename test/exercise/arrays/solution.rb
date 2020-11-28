64265148c4fad6f6c4e6a75afea36ed7c6cce05e
Maksim1509 <maaa23@mail.ru>
2020-11-28 22:31:32 +0400

    Made replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,8 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_num = array.max
+        array.map! { |num| num > 0 ? max_num : num }
       end
 
       def search(_array, _query)
