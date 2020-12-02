99227d2db6a077836b3e7fb90ac9b938170cffd9
Maksim1509 <maaa23@mail.ru>
2020-12-02 18:32:08 +0400

    Made rating

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,18 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        mapped = array.map do |film|
+          country_count = film['country'] ? film['country'].split(',').size : 0
+          rating = film['rating_kinopoisk'] ? film['rating_kinopoisk'].to_f : 0
+          item = {}
+          item['country_count'] = country_count
+          item['rating'] = rating
+          item
+        end
+        selected = mapped.select { |film| film['country_count'] >= 2 && film['rating'].positive? }
+        sum_rating = selected.reduce(0) { |acc, film| acc + film['rating'] }
+        sum_rating / selected.size
       end
 
       def chars_count(_films, _threshold)
