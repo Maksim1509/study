04db654b4fd7822f9ce3522203b1e7bcacd1bc8b
Maksim1509 <maaa23@mail.ru>
2020-12-03 13:09:52 +0400

    Fixed linter errors

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,18 +1,17 @@
 module Exercise
   module Arrays
     class << self
+      def get_max(max, array)
+        return max if array.empty?
+
+        first, *rest = array
+        max = first > max ? first : max
+        get_max(max, rest)
+      end
+
       def replace(array)
-        def get_max(array)
-          def iter(max, ar)
-            return max if ar.size == 0
-            first, *rest = ar
-            max = first > max ? first : max
-            iter(max, rest)
-          end
-          first, *rest = array
-          iter(first, rest)
-        end
-        max_num = get_max(array)
+        first, *rest = array
+        max_num = get_max(first, rest)
         array.map! { |num| num.positive? ? max_num : num }
       end
 
