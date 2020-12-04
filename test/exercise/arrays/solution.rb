b4894876ed36b36b84f349fff56e6d581f309fdb
Maksim1509 <maaa23@mail.ru>
2020-12-04 20:35:54 +0400

    Refactoring

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,18 +1,20 @@
 module Exercise
   module Arrays
     class << self
-      def get_max(max, array)
-        return max if array.empty?
-
+      def get_max(array)
+        iter = ->(max, arr) do
+          return max if arr.empty?
+          first, *rest = arr
+          max = first > max ? first : max
+          iter.call(max, rest)
+        end
         first, *rest = array
-        max = first > max ? first : max
-        get_max(max, rest)
+        iter.call(first, rest)
       end
 
       def replace(array)
-        first, *rest = array
-        max_num = get_max(first, rest)
-        array.map! { |num| num.positive? ? max_num : num }
+        max_num = get_max(array)
+        array.map { |num| num.positive? ? max_num : num }
       end
 
       def search(_array, _query)
